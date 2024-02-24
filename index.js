f1c86bbda12bfc6cd3f79a1297e90bd98178dc1c
Abir Rahman <129317779+Rahman2023@users.noreply.github.com>
2024-02-24 23:37:27 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,98 @@
+
+// top scroll start here
+
+const scrollTop = document.querySelector("#bttbutton")
+scrollTop.addEventListener("click", smoth);
+function smoth() {
+    window.scrollTo({top:0, behavior: 'smooth'});
+     
+}
+// top scroll end here
+
+
+// range slider start here
+
+const rangeInput = document.querySelectorAll(".range-input input"),
+priceInput = document.querySelectorAll(".price-input input"),
+range = document.querySelector(".slider .progress");
+let priceGap = 0;
+
+priceInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minPrice = parseInt(priceInput[0].value),
+        maxPrice = parseInt(priceInput[1].value);
+        
+        if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+            if(e.target.className === "input-min"){
+                rangeInput[0].value = minPrice;
+                range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+            }else{
+                rangeInput[1].value = maxPrice;
+                range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+            }
+        }
+    });
+});
+
+rangeInput.forEach(input =>{
+    input.addEventListener("input", e =>{
+        let minVal = parseInt(rangeInput[0].value),
+        maxVal = parseInt(rangeInput[1].value);
+
+        if((maxVal - minVal) < priceGap){
+            if(e.target.className === "range-min"){
+                rangeInput[0].value = maxVal - priceGap
+            }else{
+                rangeInput[1].value = minVal + priceGap;
+            }
+        }else{
+            priceInput[0].value = minVal;
+            priceInput[1].value = maxVal;
+            range.style.left = ((minVal / rangeInput[0].max) * 100) + "%";
+            range.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+        }
+    });
+});
+
+// range slider end here
+
+
+// single product list increasse and decrease
+
+      let valueCount;
+      let price = document.getElementById("price").innerText;
+
+      
+      //price calculation function
+      function priceTotal() {
+        let total = valueCount * price;
+        document.getElementById("price").innerText = total;
+      }
+
+
+
+      //plus button
+      document
+        .querySelector(".plus-btn")
+        .addEventListener("click", function () {
+          valueCount = document.getElementById("quantity").value;
+          valueCount++;
+          document.getElementById("quantity").value = valueCount;
+          priceTotal();
+        });
+
+
+
+      //plus button
+      document
+        .querySelector(".minus-btn")
+        .addEventListener("click", function () {
+          valueCount = document.getElementById("quantity").value;
+        if (valueCount > 1) {
+            valueCount--;
+            document.getElementById("quantity").value = valueCount;
+        } 
+          priceTotal();
+        });
+
+        // single product list increasse and decrease
